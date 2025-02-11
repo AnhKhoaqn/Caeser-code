2de6a0128c8f31bde3768962cc85365375bf1236
AnhKhoaqn <idappstore.qn@gmail.com>
2025-02-11 15:41:16 +0700

    caeser

---
+++ caser.py
@@ -0,0 +1,12 @@
+import string
+name="nguyen anh khoa"
+key=11
+a=list(string.ascii_lowercase)  # Tạo danh sách chữ cái từ a đến z
+b=a[key:]+a[:key]
+print("Ten duoc ma hoa: ",end='')
+for i in name:
+    if i in a:
+        print(b[a.index(i)],end='')
+    else:
+        print(i,end="")
+
